f09e3ebdc007684f740f3e874df0af8098ec20b2
popllstar <1332381368@qq.com>
2021-10-26 00:03:07 +0800

    asd

--- src/test/java/Xin_Yang/Test1.java
+++ src/test/java/Xin_Yang/Test1.java
@@ -2,15 +2,16 @@ package Xin_Yang;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-public class AppTest {
+public class Test1 {
+
     @Test
-    void testadd(){
+    public void Testadd(){
     int i = new  Calc().add();
         Assertions.assertEquals(7,i);
     }
 
     @Test
-    void subtract(){
+    public void Testsubtract(){
         int i = new  Calc().subtract();
         Assertions.assertEquals(2,i);
     }
